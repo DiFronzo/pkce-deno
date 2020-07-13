e8b2ddcef0e92422b32ada5c3d5f884635c26dcb
Andreas Lien <30557582+DiFronzo@users.noreply.github.com>
2020-07-13 18:11:01 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,25 +1,25 @@
 import { assertEquals, assertThrows } from "https://deno.land/std/testing/asserts.ts";
 import { create } from "./mod.ts";
 
-Deno.test("Test of length", () => {
-        const codePair: any = create(43);
-        assertEquals(codePair.codeChallenge.length, 43);
-});
+function test(size: number, type: string) {
+    return function() {
+        const codePair: any = create(size);
+        assertEquals(codePair[type].length, size);
+    }
+}
+
+
+Deno.test("Test of length", test(43, 'codeChallenge'));
+Deno.test("Test of length 2", test(128, 'codeVerifier'));
 
 Deno.test("Too short length", function (): void {
-  assertThrows((): void => {
+  assertThrows(() => {
     throw create(10);
   }, Error);
 });
 
 Deno.test("Too long length", function (): void {
-  assertThrows((): void => {
+  assertThrows(() => {
     throw create(130);
   }, Error);
 });
-
-Deno.test("Test of length", () => {
-        const codePair: any = create();
-        assertEquals(codePair.codeVerifier.length, 128);
-});
-
