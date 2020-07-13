4782f3505545edb74ff3a0d2fce636e6b53bd4ac
Andreas Lien <30557582+DiFronzo@users.noreply.github.com>
2020-07-13 18:14:29 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,26 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { assertEquals, assertThrows } from "https://deno.land/std/testing/asserts.ts";
 import { create } from "./mod.ts";
 
-// Simple name and function, compact form, but not configurable
-Deno.test("hello world #1", () => {
-  const x = 1 + 2;
-  assertEquals(x, 3);
+function test(size: number, type: string) {
+    return function() {
+        const codePair: any = create(size);
+        assertEquals(codePair[type].length, size);
+    }
+}
+
+Deno.test("Test of length", test(43, 'codeChallenge'));
+Deno.test("Test of length 2", test(128, 'codeVerifier'));
+
+Deno.test("Too short length", function (): void {
+  assertThrows(() => {
+    throw create(10);
+  }, Error);
 });
+
+Deno.test("Too long length", function (): void {
+  assertThrows(() => {
+    throw create(130);
+  }, Error);
+});
+
+
